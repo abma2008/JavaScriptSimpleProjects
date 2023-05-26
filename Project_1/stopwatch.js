440dbf7c7bdc09f8b11d6d59a0b3a499416f372f
abma2008 <abma2008@gmail.com>
2023-05-26 15:13:46 +0400

    StopWatch Simple JavaScript

---
+++ Project_1/stopwatch.js
@@ -0,0 +1,43 @@
+/**
+ * Creating variables to hold the position
+ * of each element we will be using for this
+ * project
+ */
+
+let seconds = document.querySelector(`#seconds`);
+let milliseconds = document.querySelector(`#milliseconds`);
+let startTimer = document.querySelector(`#start`);
+let stopTimer = document.querySelector(`#stop`);
+let resetTimer = document.querySelector(`#reset`);
+
+let secs = 0;
+let millisecs = 0;
+// creating a variable named timeinterval:
+let timeinterval;
+const timer = ()=>{
+    millisecs++;
+    millisecs < 9 ? milliseconds.innerHTML="0"+millisecs: milliseconds.innerHTML = millisecs;
+    if(millisecs > 99){
+        secs++;
+        seconds.innerHTML = "0"+secs;
+        millisecs = "0"+0;
+    }
+    secs > 9? seconds.innerHTML=secs: seconds.innerHTML = "0"+secs;
+}
+
+
+// adding event listeners to the buttons: start, stop, and reset:
+
+startTimer.addEventListener("click", ()=>{
+    timeinterval = setInterval(timer, 10);
+});
+stopTimer.addEventListener("click", ()=>{
+    clearInterval(timeinterval);
+})
+resetTimer.addEventListener("click", ()=>{
+    clearInterval(timeinterval);
+    secs=0;
+    millisecs =0;
+    seconds.innerHTML="00";
+    milliseconds.innerHTML="00";    
+})
