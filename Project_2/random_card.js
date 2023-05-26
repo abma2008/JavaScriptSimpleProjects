19e1d510fdfc939376cba3838cc35606392b4715
abma2008 <abma2008@gmail.com>
2023-05-26 15:47:40 +0400

    Changing the stopwatch.css background and color of buttons adding Project_2 Tutorial with Files

---
+++ Project_2/random_card.js
@@ -0,0 +1,13 @@
+/**
+ * This is a JavaScript Exercise where we are using a card
+ * and displaying random data from a list created
+ * we will be using three buttons and also, initiate the
+ * onload to display the first item inside a list.
+ * please keep this as a reference in the future.
+ */
+
+
+let title = document.querySelector(`#title`);
+let img = document.querySelector(`#img`)
+let body = document.querySelector(`#body`);
+let footer = document.querySelector(`#footer`);
