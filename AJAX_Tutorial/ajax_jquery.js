591a763406ba0c90acbfe86c276c54edf554d824
abma2008 <abma2008@gmail.com>
2023-12-15 21:21:10 +0400

    edting the ajax_tutorial.md file

--- AJAX_Tutorial/ajax_jquery.js
+++ AJAX_Tutorial/ajax_jquery.js
@@ -1,17 +1,20 @@
-$(()=>{
+// this is a direct usage of JQuery without the old syntax.
+// Please make sure to view them :)
+
+$(() => {
     // This is the button handling hiding the container class from the page:
-    $(`.container`).on(`dblclick`, ()=>{
+    $(`.container`).on(`dblclick`, () => {
         $(`.container`).slideUp(2000);
         $(`#show-container`).slideDown(2000);
     })
     // This is the button in the header handling showing back the container div:
-    $(`#show-container`).on(`click`, ()=>{
+    $(`#show-container`).on(`click`, () => {
         $(`#show-container`).slideUp(2000);
         $(`.container`).slideDown(2000);
     })
     // Creating a function for posting data to ajax.json file:
-    let postData = ()=>{
-        if($(`#firstname`).val() ==`` || $(`#lastname`).val() ==`` || $(`#phone`).val() ==`` || $(`#email`).val()==``){
+    let postData = () => {
+        if ($(`#firstname`).val() == `` || $(`#lastname`).val() == `` || $(`#phone`).val() == `` || $(`#email`).val() == ``) {
             Swal.fire({
                 icon: `info`,
                 title: `Missing Data`,
@@ -19,30 +22,30 @@ $(()=>{
             })
             $(`#firstname`).focus();
         }
-        else{
+        else {
             // This is the part of ajax that posts the data into the items json file.
             $.ajax({
-            method: `POST`,
-            url: `http://localhost:3000/items`,
-            data:{
-                firstname: $(`#firstname`).val(),
-                lastname: $(`#lastname`).val(),
-                phone: $(`#phone`).val(),
-                email: $(`#email`).val()
-            },
-            success: function(data, status){
-                console.log(data, status)
-                alert(`The data has been saved successfully`)
-            },
-            error: function(err, status){
-                console.log(err, status)
-            }
-        })
+                method: `POST`,
+                url: `http://localhost:3000/items`,
+                data: {
+                    firstname: $(`#firstname`).val(),
+                    lastname: $(`#lastname`).val(),
+                    phone: $(`#phone`).val(),
+                    email: $(`#email`).val()
+                },
+                success: function (data, status) {
+                    console.log(data, status)
+                    alert(`The data has been saved successfully`)
+                },
+                error: function (err, status) {
+                    console.log(err, status)
+                }
+            })
         }
     }
 
     // creating a function to getData:
-    let getData = function(){
+    let getData = function () {
         let table = document.querySelector(`#data-table`);
         let table_head = `
         <tr>
@@ -59,16 +62,16 @@ $(()=>{
         $.ajax({
             method: `GET`,
             url: `http://localhost:3000/items`,
-            data:{
+            data: {
                 items: `items`
             },
-            success: function(items, status){
+            success: function (items, status) {
                 // console.table(items)
                 // console.log(status)
-                if(items.length !=0){
+                if (items.length != 0) {
                     table.innerHTML = table_head;
-                    items.forEach(item =>{
-                        let table_row =`
+                    items.forEach(item => {
+                        let table_row = `
                         <tr>
                         <tbody>
                         <td>${item.id}</td>
@@ -88,87 +91,87 @@ $(()=>{
                     $(`#show-data`).html(``);
                 }
             },
-            error: function(err){
+            error: function (err) {
                 console.log(err);
             }
         })
     }
 
     //editButton ........ updateData():
-    let updateData = (id)=>{
+    let updateData = (id) => {
         $.ajax({
             method: `PUT`,
             url: `http://localhost:3000/items/${id}`,
-            data:{
+            data: {
                 firstname: prompt(`Enter First Name: `),
                 lastname: prompt(`Enter Last Name: `),
                 phone: prompt(`Enter Phone Number: `),
                 email: prompt(`Enter Email Address: `),
             },
-            success: function(data, status){
+            success: function (data, status) {
                 alert(`The Item has been updated...... ID: ${id}`)
             },
-            error: function(){
+            error: function () {
                 alert(`Something went wrong`)
             }
         })
     }
-    
+
     // deleteButton.....deleteData():
-    let deleteData = (id)=>{
+    let deleteData = (id) => {
         Swal.fire({
             icon: `warning`,
             title: `ID: ${id}`,
             text: `are you sure you want to delete item ID: ${id}?`,
-            showConfirmButton:true,
+            showConfirmButton: true,
             showCancelButton: true,
-        }).then((result)=>{
-            if(result.isConfirmed){
+        }).then((result) => {
+            if (result.isConfirmed) {
                 // This is the part of ajax that deletes the item from the table:
                 $.ajax({
                     method: `DELETE`,
                     url: `http://localhost:3000/items/${id}`,
-                    success: function(){
+                    success: function () {
                         alert(`The Item has been deleted successfully..... ID: ${id}`)
                     },
-                    error: function(){
+                    error: function () {
                         alert(`Something went wrong.....`)
                     }
-                    })
+                })
             }
         })
     }
 
     // Delete all Data by clicking the button deleteButton:
-    let deleteAllData = ()=>{
+    let deleteAllData = () => {
         Swal.fire({
             icon: `warning`,
             title: `Delete All`,
             text: `Are you Sure you want to proceed with this action?`,
             showConfirmButton: true,
             showCancelButton: true,
-        }).then((result)=>{
-            if(result.isConfirmed){
+        }).then((result) => {
+            if (result.isConfirmed) {
                 // this is the part that handles removing all items:
                 $.ajax({
                     method: `GET`,
                     url: `http://localhost:3000/items`,
-                    success: function(data, status){
-                        if(data.length !=0){
-                            data.forEach(item=>{
+                    success: function (data, status) {
+                        if (data.length != 0) {
+                            data.forEach(item => {
                                 $.ajax({
                                     method: `DELETE`,
                                     url: `http://localhost:3000/items/${item.id}`,
-                                    success: function(){},
-                                    error: function(){}
+                                    success: function () { },
+                                    error: function () { }
                                 })
                             })
                         }
-                        else{
+                        else {
                             alert(`No Data to delete`)
                         }
                     },
-                    error: function(){
+                    error: function () {
                         alert(`something went wrong`)
                     }
                 })
@@ -178,22 +181,22 @@ $(()=>{
 
 
     // Assigning the function postData to the button postButton:
-    $(`#postButton`).on(`click`, function(){
+    $(`#postButton`).on(`click`, function () {
         postData();
     })
 
     // Assigning the function getData to the button getButton:
-    $(`getButton`).on(`click`, ()=>{
+    $(`getButton`).on(`click`, () => {
         getData();
     })
 
     // using document event to select the target:
-    document.addEventListener(`click`, function(e){
-        if(e.target.classList.contains(`update`)){
+    document.addEventListener(`click`, function (e) {
+        if (e.target.classList.contains(`update`)) {
             let get_id = e.target.getAttribute(`id`)
             updateData(get_id);
         }
-        else if(e.target.classList.contains(`del`)){
+        else if (e.target.classList.contains(`del`)) {
             let get_id = e.target.getAttribute(`id`)
             deleteData(get_id);
         }
@@ -201,17 +204,17 @@ $(()=>{
 
 
     // adding onclick to deleteButton:
-    $(`#deleteButton`).on(`click`, function(){
+    $(`#deleteButton`).on(`click`, function () {
         deleteAllData();
     })
 
     // hideShowBtn Functionality:
-    $(`#hideShowBtn`).on(`click`, function(){
+    $(`#hideShowBtn`).on(`click`, function () {
         $(`.show-data`).slideToggle(3000);
-        $(`#hideShowBtn`).text() ===`Hide Table`? $(`#hideShowBtn`).text(`Show Table`): $(`#hideShowBtn`).text(`Hide Table`)
+        $(`#hideShowBtn`).text() === `Hide Table` ? $(`#hideShowBtn`).text(`Show Table`) : $(`#hideShowBtn`).text(`Hide Table`)
     })
     // onload functionality:
-    window.onload = ()=>{
+    window.onload = () => {
         getData();
         $(`#firstname`).focus();
     }
--- AJAX_Tutorial/ajax_tutorial.md
+++ AJAX_Tutorial/ajax_tutorial.md
@@ -2,3 +2,8 @@
 ## Tutorial AJAX:
 ### In this tutorial, we will be learning the basics of AJAX.
 ### we will try to cover all the basic operations to develop some understanding.
+# Extremely important Tip to keep in mind for this tutorial:
+* we are using json-server to run this applicaton.
+* you must have Nodejs installed on your machine.
+* you must also have json-server package installed globally.
+* the command to run: json-server --watch ajax.json
